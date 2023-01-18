82c01510a9b90c74220797c39ad1057b1efb5a60
ImzyO <100032916+ImzyO@users.noreply.github.com>
2023-01-18 00:01:48 -0800

    mongodb

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+a Python function that changes all topics of a school document based on the name:
+"""
+def update_topics(mongo_collection, name, topics):
+    return mongo_collection.update_many({"name": name},
+            {"$set": {"topics": topics}})
